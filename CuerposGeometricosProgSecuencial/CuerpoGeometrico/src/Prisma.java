261b617d8426b00f1baf4bc88656cd6a771b2aa4
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-12 14:46:46 -0500

    Add files via upload

---
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Prisma.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Prisma {
+    public static void main(String[] args) throws Exception {
+     Scanner lector = new Scanner(System.in);
+      
+         //Declaramos las variables del prisma
+        double base;
+        double area;
+        double altura;
+
+        //double volumen;
+       //Pedimos los datos de entrada
+       System.out.println("Ingrese la base del prisma: ");
+       base = lector.nextDouble();
+
+       System.out.println("Ingrese el area el prisma: ");
+       area = lector.nextDouble();
+
+
+       System.out.println("Ingrese la altura del prisma: ");
+       altura = lector.nextDouble();
+
+       //Hacemos las operaciones 
+        //volumen = base *  area * altura /2;
+        System.out.println( "El volumen de un prisma es: " + base * area * altura /2 );
+        lector.close();
+
+    }
+}
