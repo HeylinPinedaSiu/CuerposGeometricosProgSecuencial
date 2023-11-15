eab6e6606a7b01f0768a05795cb6f327e8f2de64
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 20:00:36 -0500

    HuesoEsfericoYCuñaEsferica.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/HuesoEsfericoYCuñaEsferica.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/HuesoEsfericoYCuñaEsferica.java
@@ -1,30 +1,33 @@
 import java.util.Scanner;
-public class HuesoEsfericoYCuñaEsferica {
 
-    public static void main(String[] args) throws Exception {
+public class HuesoEsfericoYCuñaEsferica{
+   
+    public static void main(String []args){
         Scanner lector = new Scanner(System.in);
 
-     //declaracion de variables
-	double  Area;
-    double  Volumen;
-    double  n;
-    double  radio;
+        //declaramos las variables 
+       int angulo;
+       double radio;
+       double area;
+       double  volumen;
 
-	//solicitar las variables
-	System.out.println("Ingrese el radio:");
-    radio = lector.nextDouble();
+       //pedimos los datos al usuario 
+       System.out.print("Ingrese el valor del radio: ");
+       radio = lector.nextDouble();
+	    
+       System.out.print("ingrese el valor del angulo: ");
+       angulo = lector.nextInt();
 
-    //Proceso //area
-    n = 15;
-	Area = (4 * Math.PI * (radio * radio ) * n) / 360;
+       //procesamos los datos 
+       area = 4*(Math.PI*(radio*radio)*angulo/360);
+       volumen = (4.0/3.0)*(Math.PI*(radio*radio*radio)*angulo/360);
 
-	//volumen
-	Volumen = (4 * Math.PI * (radio * radio * radio) * n) / 1080;
+       //mostramos los resultados 
+       System.out.println("El area del uso esferico es: "+area);
+       System.out.println("El volumen de una cuña esferica es: "+volumen);
 
-	//resultado
-	System.out.println( "El Area del huso esferico es: "+Area);
-	System.out.println ("El volumen del huso esferico es: "+Volumen);
+       lector.close();
 
-    lector.close();
-}
+    }
+
 }
