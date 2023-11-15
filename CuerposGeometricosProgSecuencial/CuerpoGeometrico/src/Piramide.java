4186d2d9d577f5c50e9700b483ee7e7b2c060a33
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 20:15:39 -0500

    Piramide.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Piramide.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Piramide.java
@@ -1,48 +1,54 @@
 import java.util.Scanner;
-public class Piramide {
-   public static void main(String[] args) throws Exception {
+ 
+public class Piramide{
+ public static void main(String[] args) throws Exception {
         Scanner lector = new Scanner(System.in);
 
-     //Declaramos las variables
-     double altura;
-    double base;
-    double areabase;
-     //double perimetral;
-     double lado;
-     //double pbase;
-   double areaLateral;
-   double AreaTriangulo;
-    double areatotal;
-    double volumen;
+        //declaramos las variables
+        int lados;
+        double areaLateral;
+        double  areaTotal;
+        double volumen;
+        double altura;
+        double apotemaPiramide;
+        double perimetroBase;
+        double Apotema;
+        double areaBase;
+        double longLados;
 
-    //pedimos los datos de entrada
+        //pedimos los datos al ussuario 
+        System.out.print("Ingrese la altura de la pirámide: ");
+        altura = lector.nextDouble();
 
-    System.out.println("Ingrese la base de la piramide : ");
-    base = lector.nextDouble();
+        System.out.print("Ingrese la cantidad de lados de la pirámide: ");
+        lados = lector.nextInt();
 
-    System.out.println("Ingrese la altura de la piramide: ");
-    altura = lector.nextDouble();
+        System.out.print("Ingrese el apotema de la pirámide: ");
+        apotemaPiramide = lector.nextDouble();
 
-     System.out.println("Ingrese la altura longitud de lado  de la piramide : ");
-     lado  = lector.nextDouble();
-     
-     //proceso
-     //area lateral
-     base = lado;
-     AreaTriangulo = base* altura /2;
-     areaLateral = 4* AreaTriangulo;
-     
-    //area total
-     areabase = (base * altura) /2;
-    areatotal = areaLateral + areabase;
-    // volumen
-    volumen = (areabase * altura) / 3;
-    
-    System.out.println("El area Lateral de la piramide es : " +areaLateral);
-    System.out.println("El area total de la piramide es: " +areatotal);
-    System.out.println("El volumen de la piramide es: " + volumen);
-      
-    lector.close();
+        System.out.print("Ingrese la Longitud de los lados : ");
+        longLados = lector.nextDouble();
 
-   }
+        System.out.print("Ingrese el apotema de la base: ");
+        Apotema = lector.nextDouble();
+
+        //procesamos los datos
+        perimetroBase = lados * longLados;
+        apotemaPiramide = (Math.sqrt(altura*altura)+(Apotema*Apotema));
+        areaLateral = (perimetroBase*apotemaPiramide)/2;
+        areaBase = perimetroBase*2*(Apotema+apotemaPiramide);
+        areaTotal = (areaLateral+areaBase);
+        volumen = (areaBase+altura)/3;
+        
+        //mostramos los resukltados en pantalla
+       System.out.println("El area Lateral de la pirámide es: "+areaLateral);
+       System.out.println("El area total de la pirámide es "+areaTotal);
+       System.out.println("El volumen de la pirámide es: "+volumen);
+
+       lector.close();
+
+
+
+    }
+
 }
