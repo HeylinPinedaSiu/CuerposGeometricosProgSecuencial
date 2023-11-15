3cb655cd539aca92e4a080f26b8edfcaaf4052ae
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 20:06:04 -0500

    Icosaedro.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Icosaedro.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Icosaedro.java
@@ -1,28 +1,30 @@
 import java.util.Scanner;
-public class Icosaedro {
-    public static void main(String[] args) throws Exception {
+
+public class Icosaedro{
+   
+    public static void main(String []args){
         Scanner lector = new Scanner(System.in);
 
-            //declaracion de variables
-	  double area_1;
-      double Volumen;
-     double lado;  
-	
-	    //solicitar las variables
-	    System.out.println("Digite la longitud lado: ");
-	    lado = lector.nextDouble();
+		//declaramos las variables
+        int arista;
+        double areaTotal;
+		double areaCara;
+		double volumen;
 
-	    //Proceso de area 
-	    area_1 = 5* lado * lado * Math.sqrt(3);
+		//pedimos los datos al usuario
+        System.out.print("Ingrese el valor de la arista: ");
+        arista = lector.nextInt();
 
-	    //volumen
-	    Volumen  = (5*(3 + Math.sqrt(5))*(lado * lado * lado ))/12;
+		//hacemos las operaciones
+        areaTotal = (5*(arista*arista)*Math.sqrt(3));
+        areaCara = areaTotal/20;
+        volumen = ((5.0/12)*(3+ Math.sqrt(5)*arista*arista*arista));
 
-	    //resultado
-	    System.out.println("El area del Icosaedro es: "+ area_1);
-	    System.out.println("El volumen del Icosaedro es: "+Volumen);
-    
-     lector.close();
-    
-}
+		//mostramos los resultados 
+        System.out.println("El area Toltal del Icosaedro es: " + areaTotal );
+        System.out.println("Las caras del Icosaedro son : "+areaCara);
+        System.out.println("El volumen del Icosaedro es: "+volumen);  
+  
+		lector.close();
+	}
 }
