6360df1641e2e6b0d9f0a8df6e341ce621ec0b8c
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 19:21:33 -0500

    Cilindro.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Cilindro.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Cilindro.java
@@ -6,30 +6,33 @@ public class Cilindro {
          //Declaramos las variables
          double radio;
          double altura;
-         //double base;
-         double area;
-        double volumen;
-
+         double  areaLateral;
+         double areaTotal;
+         double Volumen;
+         double perimetroBase;
+         double areaBase;
+        
+      
         //pedimos los datos de entrada
-
-        System.out.println("Ingresea el  radio del cilindro: ");
-        radio = lector.nextDouble();
-
          System.out.println("Ingrese la altura del cilindro: ");
         altura = lector.nextDouble();
 
-        System.out.println("Ingrese la area del cilindro: ");
-         area = lector.nextDouble();
+        System.out.println("Ingrese el radio del cilindro: ");
+        radio = lector.nextDouble();
 
         //hacemos las operaciones correspondientes
-        area = ((2) * (3.1416) * (radio)) * (altura * radio);
-        volumen = ((3.1416) * (radio * radio ) * altura );
+        perimetroBase = 2*Math.PI*radio;
+        areaLateral = perimetroBase*altura;
+        areaBase= Math.PI* (radio*radio);
+        areaTotal = 2*Math.PI*radio*(altura + radio);
+        Volumen = areaBase*altura;
 
         //Imprimimos los resultados
-        System.out.println("El area del cilindro es : " + area+ "cm2");
-        System.out.println("El volumen del cilindro es :" + volumen+ "cm3" );
+        System.out.println("El Area Lateral del cilindro es: " + areaLateral);
+      System.out.println("El Area Total del cubo es: " + areaTotal );
+      System.out.println("El volumen del cubo es: " + Volumen );
 
-        lector.close();
-}
+       lector.close();
 }
 
+}
