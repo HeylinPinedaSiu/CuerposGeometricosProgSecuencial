399b1556d479ec7268de2e9160af14b2acabadf9
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 19:32:04 -0500

    Hexaedro.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Hexaedro.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Hexaedro.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
-public class Hexaedro {
-     
-    public static void main(String[] args) throws Exception {
-        Scanner lector = new Scanner(System.in);
 
-      //declaracion de variables
-	double Area_1;
-   double  Volumen;
-   double  Diagonal;
-   double  ar2; 
+public class Hexaedro{
 
-   //solicitar las variables
-   System.out.println("Ingrese el valor de la longitud de lado: ");
-   ar2 = lector.nextDouble();
+    public static void main(String [] args) throws Exception{
+    Scanner lector = new Scanner(System.in);
 
-   //Proceso 
-   Area_1  = 6*(ar2 * ar2);
-   //volumen
-   Volumen = ar2  * ar2 * ar2;
+    //declaramos las variables 
+    int Arista;
+    double area;
+    double volumen;
+    double diagonal;
 
-   //diagonal
-   Diagonal = ar2 * Math.sqrt(3);
-   
-   //resultado
-   System.out.println("El area del hexaedro es: "+Area_1);
-   System.out.println("volumen del hexaedro es: "+ Volumen);
-   System.out.println("La diagonal del hexaedro es: "+Diagonal);
+    //pedimos los datos al usuario 
+    System.out.print("Ingrese el valor de la arista del Hexaedro: ");
+    Arista = lector.nextInt();
 
-   lector.close();
+    //hacemos las operaciones 
+    area = (6*(Arista*Arista));
+    volumen = (Arista*Arista*Arista);
+    diagonal = (Arista*Math.sqrt(3));
 
-}
+    //mostramos los resultados 
+    System.out.println("El area del Hexaedro es: "+area); 
+    System.out.println("El volumen del Hexaedro es: "+volumen);
+    System.out.println("La diagonal del Hexaedro es: "+area);
+    
+      lector.close();
+
+    }
+
+
 }
