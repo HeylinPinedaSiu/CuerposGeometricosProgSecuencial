03a6eb1b6300879189e55a44d4dfdd0da8611fa8
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 19:23:13 -0500

    Cono.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Cono.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Cono.java
@@ -7,33 +7,39 @@ public class Cono {
      double radio;
      double generatriz;
      double altura;
-     double areabase;
-     double arlateral;
+     double perimetroBase;
+     double areaLateral;
+     double areaBase;
      double areaTotal;
-     double volumen;
+     double Volumen;
       
      //pedimos los datos de entrada al usuario 
      System.out.println("Ingrese el radio del cono: ");
      radio = lector.nextDouble();
 
-     System.out.println("Ingrese la genratriz del cono: ");
-     generatriz = lector.nextDouble();
-
      System.out.println("Ingrese la altura del cono: ");
      altura = lector.nextDouble();
 
      //hacemos las operaciones
-     areabase = (3.1416) * Math.pow(radio, 2);
-     arlateral = (3.1416) * radio * generatriz;
-     areaTotal = areabase + arlateral;
-     volumen = (areabase * altura )/3;
+     perimetroBase = (Math.PI*radio);
+     generatriz = Math.sqrt((radio*radio)+(altura*altura));
+     areaLateral = perimetroBase*generatriz;
+     areaBase = (Math.PI*(radio*radio));
+     areaTotal = areaLateral + areaBase;
+     Volumen = areaBase*altura/3;
+ 
 
      //mandamos a imprimir los resultados
-      System.out.println("El area base es: " + areabase);
-     System.out.println("El area Lateral es: " + arlateral);
+      System.out.println("El area base es: " + areaBase);
+     System.out.println("El area Lateral es: " + areaLateral);
      System.out.println("El area Total es: " + areaTotal);
-     System.out.println("El volumen es: " + volumen);
+     System.out.println("El volumen es: " + Volumen);
 
+     //mostramos los datos 
+     System.out.println("El area lateral el cono es: "+ areaLateral);
+     System.out.println("El area total del cono es: "+ areaTotal);
+     System.out.println("el volumen del cono es: "+ Volumen );
+   
      lector.close();
 
     
