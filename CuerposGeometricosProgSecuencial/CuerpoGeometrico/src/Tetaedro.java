12a33f1a07aad2e1b7f4e151060147c29f97344e
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 21:21:31 -0500

    Tetaedro.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Tetaedro.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/Tetaedro.java
@@ -1,33 +1,30 @@
 import java.util.Scanner;
-public class Tetaedro {
-    public static void main(String[] args) throws Exception {
-     Scanner lector = new Scanner(System.in);
 
-    //declaracion de variables
-	//double area;
-    //double A2;
-    double A1;
-    double  Volumen;
-    double altura;
-     double A; 
- 
-     //solicitar las variables
-     System.out.println("Digite la longitud de lado: ");
-     A1 = lector.nextDouble();
- 
-     //Proceso 
-     A = A1 * A1 * A1  * Math.sqrt(3);
- 
-     //volumen
-     Volumen = (Math.sqrt(2) * (A1 * A1 * A1 ))/3;
- 
-     //altura
-     altura = (A1  * Math.sqrt(6)) / 3;
- 
-     //resultado
-     System.out.println("El area del tetraedro es: " + A);
- 
-     lector.close();
+public class Tetaedro{
+
+    public static void main(String [] args){
+    Scanner lector = new Scanner(System.in);
+
+    //declaramos las variables 
+    int arista;
+    double altura,Area,volumen;
+
+    //pedimos los datos al usuario 
+   System.out.print("Ingrese el valor de la arista: ");
+   arista = lector.nextInt();
+
+   //procesamos los datos 
+   Area = ((arista*arista)*Math.sqrt(3));
+   altura = (arista*(Math.sqrt(6)/3));
+   volumen = ((Math.sqrt(2)/12)*(arista*arista*arista));
+
+   //mosytramos en pantalla
+   System.out.println("El valor del area es: "+Area);
+   System.out.println("El valor de la altura es: "+altura);
+   System.out.println("El valor del volumen es: "+volumen);
+
+   lector.close();
   }
+
 
 }
