c0cf69df2ef3842784afbd5baddfbe0130916a50
HeylinPinedaSiu <142961091+HeylinPinedaSiu@users.noreply.github.com>
2023-11-14 19:20:00 -0500

    CasqueteEsferico.java

--- CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/CasqueteEsferico.java
+++ CuerposGeometricosProgSecuencial/CuerpoGeometrico/src/CasqueteEsferico.java
@@ -1,34 +1,45 @@
 import java.util.Scanner;
-public class CasqueteEsferico { 
-    public static void main(String[] args) throws Exception {
-        Scanner lector = new Scanner(System.in);
 
-    //solicitar variables del casquete
-    double  A_casquete;
-    // double altura;
-    double altura_c;
-    double Radio_c;
-    double radio_c;
-    double Volumen_casquete;
+public class CasqueteEsferico{
 
-      //solicitamos los datos de entrada
-     System.out.println( "Digite la altura del casquete: ");
-     altura_c = lector.nextDouble();
-     
-     //Area del casquete
-     radio_c = altura_c;
-     Radio_c = (radio_c*radio_c + altura_c*altura_c) / (2*altura_c);
-     A_casquete = 2 * Math.PI * Radio_c * altura_c;
- 
-     //volumen del casquete
-     Volumen_casquete = (Math.PI * (altura_c*altura_c) * ((3*Radio_c) - altura_c)) / 3;
-     
-     //resultados del casquete
-     
-     System.out.println ("El area del casquete es: "+A_casquete);
-     System.out.println ("volumen del casquete es: "+ Volumen_casquete);
-     
-     lector.close();
-    
+    public static void main(String [] args){
+    Scanner lector = new Scanner(System.in);    
+
+
+  //declaramos las variables 
+   double area;
+   double Volumen;
+   double  volumem;
+   double radio;
+   double radioM;
+   double altura;
+   double aream;
+
+   //pedimos al usuiario los datos 
+   System.out.print("Ingrese la altura: ");
+   altura = lector.nextDouble();
+
+   System.out.print("Ingrese el radio Mayor: ");
+   radioM = lector.nextDouble();
+
+   System.out.print("Ingrese el radio menor: ");
+   radio = lector.nextDouble();
+
+    //hacemos las operaciones correspondientes 
+    area = 2*Math.PI*radioM*altura;
+    aream = Math.PI*((radio*radio)+(altura*altura));
+    Volumen = Math.PI*(altura*altura)*(3*radioM-altura)/3;
+    volumem = Math.PI*altura/6*(3*(radio*radio)+(altura*altura));
+
+    //mostramos los resultados 
+    System.out.println("Area en funcion del radio de la esfera: "+area);
+    System.out.println("Area en funcion del radio menor: "+aream);
+    System.out.println("volumen en funcion del radio de la esfera: "+Volumen);
+    System.out.println("Area en funcion del radio menor: "+volumem);
+
+    lector.close();
+
 }
+
 }
+   
